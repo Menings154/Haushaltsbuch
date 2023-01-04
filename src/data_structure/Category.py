0db9fdb5bd53a112e0dfb59c4d2d96ce39ca0221
menings windows <menings.git@gmail.com>
2023-01-04 22:19:06 +0100

    Category added.

--- src/data_structure/Category.py
+++ src/data_structure/Category.py
@@ -1,2 +1,11 @@
+from Transaction import Transaction
+
 class Category:
-    pass
+    def __init__(self, name):
+        self.name = name
+        self.members = []
+    
+    def add_member(self, trnsctn):
+        if not isinstance(trnsctn, Transaction):
+            raise ValueError("Error while adding Transaction to a category...")
+        self.members.append(trnsctn)
