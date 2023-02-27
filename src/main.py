8c3866c5a8c1bf7a6f63a998994f2be0396637e1
Menings <menings.git@gmail.com>
2023-02-27 22:11:37 +0100

    main reading function written, but not tested

--- src/main.py
+++ src/main.py
@@ -5,24 +5,32 @@ path = r"C:\Users\Benja\Code\Python\Finanzen\Haushaltsbuch\data\input\202201\202
 
 Reader = VRReader(path)
 
+def read(reader):
+    spmonth = SpecificMonth.SpecificMonth(name=str(reader.month)+str(reader.year), 
+                                          month=reader.month, year=reader.year)
+    spmonth.add_start_balance(reader.alter_Kontostand)
+    spmonth.add_final_balance(reader.neuer_Kontostand)
+    for count, value in enumerate(reader.output):
+        name = Namer.NamerInstance.name(value[0])
+        trnsctn = Transaction.Transaction(name=name, value = value[1],
+                                          day=value[2], month=value[3],
+                                          year=value[4])
+        trnsctn.add_category(Categorizer.CategorizerInstance.categorize(trnsctn))
+        
+        for member in Day.Days.members:
+            if member.day == trnsctn.day:
+                member.add_member(trnsctn)
+        for member in Month.Months.members:
+            if member.month == trnsctn.month:
+                member.add_member(trnsctn)
+        for member in Year.Years.members:
+            if member.year == reader.year:
+                member.add_member(trnsctn)
+        for member in Month.Months.members:
+            if member.month == trnsctn.month:
+                member.add_member(trnsctn)
+        
+        DayOfTheWeek.add_to_day_of_the_week(trnsctn=trnsctn)
+        spmonth.add_member(trnsctn=trnsctn)
+    return
 print('finished')
-# def read_month()
-# for i in range(2):
-#     test_name = Namer.NamerInstance.name(Reader.output[i][0])
-#     test_trns = Transaction.Transaction(name=test_name, 
-#                                         value=Reader.output[i][1],
-#                                         day=Reader.output[i][2],
-#                                         month=Reader.output[i][3],
-#                                         year=Reader.output[i][4])
-#     test_trns.add_category(Categorizer.CategorizerInstance.categorize(test_trns))
-#     print(test_trns.category)
-#     print(Categorizer.CategorizerInstance.lut)
-#     print([member.name for member in Category.Categories.members])
-
-# print("Categorizer lut >", Categorizer.CategorizerInstance.lut)
-
-
-# print(test_trns.category)
-# print(Categorizer.CategorizerInstance.lut)
-# print([member.name for member in Category.Categories.members])
-# nun müssen die monate und so hinzugefüft werden
